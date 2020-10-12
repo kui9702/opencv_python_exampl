a55a4c4600d81565b59d45bc1c68cb6249f4b27e
kui9702 <kkk>
2020-10-12 11:06:52 +0800

    11

---
+++ face_check/generate_facedata.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def detect():
+    # 声明face_cascade变量，该变量为CascadeClassifier对象，负责人脸检测
+    face_cascade = cv2.CascadeClassifier(
+        r'face_check\haarcascade_frontalface_default.xml')
+    img = cv2.imread(  # 读取图片
+        r'face_check/22.jpg')
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # 将图片转化为灰度图
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)  # 进行实际的人脸检测
+    for (x, y, w, h) in faces:          #画出人脸矩阵
+        img = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        f = cv2.resize(gray[y:y+h, x:x+2],(200,200))
+        cv2.imwrite('')
+    cv2.imshow('Vikongs Detected!!', img)
+    cv2.imwrite(r'face_check/aa.jpg', img)
+    cv2.waitKey(0)
+
+
+if __name__ == "__main__":
+    detect()
+
